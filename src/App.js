32e1b60b5c4242ad2ef8aac5126b764ce0d4a4cc
Yevhen <lyunkor@gmail.com>
2019-10-21 14:02:57 +0300

    first comit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,64 @@ import React from 'react';
 
 function App() {
   return (
-   <> Hello from app </>
+  
+     <header>
+       <div className="wrapper-header">
+         <div className="container">
+           <section className="section-header">
+             <a href="/" className="logo"><img src="./data/backgroundWallpeper.jpg" alt="logo"/></a>
+             <ul className="main-menu">
+               <li className="main-menu__item">
+                 <a href="/" className="main-menu__link">
+                   <i className="main-menu__link-icon"></i>
+                    <span className="main-menu__link-title">name</span>
+                  </a>
+               </li>
+               <li className="main-menu__item">
+                 <a href="/" className="main-menu__link">
+                   <i className="main-menu__icon"></i>
+                   <span className="main-menu__link-title">cart</span>
+                 </a>
+               </li>
+               <li className="main-menu__item">
+                 <a href="/" className="main-menu__item">
+                   <i className="mein-menu__icon"></i>
+                   <span className="main-menu__link-title">wishes</span>
+                 </a>
+               </li>
+             </ul>
+             <ul className="info-menu">
+               <li className="info-menu__item">
+                 <a href="/" className="info-menu__item">
+                   <i className="info-menu__icon"></i>
+                   <span className="info-menu__title">contacts</span>
+                 </a>
+               </li>
+               <li className="info-menu__item">
+                 <a href="/" className="info-menu__item">
+                   <i className="info-menu__icon"></i>
+                   <span className="info-menu__title">delivery/payment</span>
+                 </a>               
+               </li>
+               <li className="info-menu__item">
+                 <a href="/" className="info-menu__item">
+                   <i className="info-menu__icon"></i>
+                   <span className="info-menu__title">faq</span>
+                 </a>                 
+                </li>
+             </ul>
+           </section>
+         </div>
+       </div>
+       <a href="" className="img"></a>
+
+     </header>,
+     <main>
+
+     </main>,
+     <footer>
+
+     </footer>
   );
 }
 
---
+++ src/Components/App/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+<header>
+    <div className='wrapper'>
+      <div className="container">
+        <aside>
+          <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+          </ul>
+        </aside>
+        <a href="">zzz</a>
+      </div>
+    </div>
+</header>
+  );
+}
+export default App;
